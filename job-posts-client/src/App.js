217294def5c15979029c9972951c55098e5baecf
jpelegrino1010 <julio.pelegrino@viseo.com>
2024-04-30 23:30:36 -0400

    initial design

--- job-posts-client/src/App.js
+++ job-posts-client/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/style.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <header>HEADER</header>
+      <main>CONTENT</main>
+      <section className="job-search">SEARCH SEACTION</section>
+      <footer>FOOTER</footer>
     </div>
   );
-}
+};
 
 export default App;
