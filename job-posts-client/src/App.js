d9535d3b7ca1bcacee4a4698a1320c82f96d6cf1
jpelegrino1010 <julio.pelegrino@viseo.com>
2024-05-01 23:35:05 -0400

    Header styles

--- job-posts-client/src/App.js
+++ job-posts-client/src/App.js
@@ -3,7 +3,29 @@ import './css/style.css';
 const App = () => {
   return (
     <div className="container">
-      <header>HEADER</header>
+      <header>
+        <section className="header-left">
+          <div className="header-left-item">
+            <img src="#" alt="logo" />
+            <h1>Prospectus</h1>
+          </div>
+          <nav>
+            <ul>
+              <li>Home</li>
+              <li>Employers</li>
+              <li>Freelancers</li>
+              <li>Pages</li>
+              <li>Blog</li>
+              <li>Support</li>
+            </ul>
+          </nav>
+        </section>
+
+        <section className="header-right">
+          <a href="#">Sing In</a>
+          <a href="#">Sign Up</a>
+        </section>
+      </header>
       <main>CONTENT</main>
       <section className="job-search">SEARCH SEACTION</section>
       <footer>FOOTER</footer>
