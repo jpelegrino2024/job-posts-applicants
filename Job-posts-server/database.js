48e251b4e68bbd7c97e8429720bf4706653649b5
jpelegrino1010 <julio.pelegrino@viseo.com>
2024-05-11 11:31:31 -0400

    test aws db

--- Job-posts-server/database.js
+++ Job-posts-server/database.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2');
 
 const dbPool = mysql
   .createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    host: 'job-posts.cub1cwlhadea.us-east-1.rds.amazonaws.com', //process.env.DB_HOST,
+    port: '3306', //process.env.DB_PORT,
+    user: 'admin', //process.env.DB_USER,
+    password: 'pass123456', //process.env.DB_PASSWORD,
+    database: 'job_posts_db' //process.env.DB_DATABASE
   })
   .promise();
 
